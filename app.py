a89995323fab45ecfc9f23300a67abdfff0a8227
njrinker <njrinker7@gmail.com>
2023-08-10 11:40:33 -0700

    Added row iteration

--- app.py
+++ app.py
@@ -13,14 +13,18 @@ openai.api_key = config['OPENAI_API_KEY']
 # Fetch the file to be operated on from the command line and convert it into a pandas dataframe
 file_in = sys.argv[1]
 df = pd.read_csv(file_in, header=None).tail(-1)
+df.replace(',','', regex=True, inplace=True)
+df.apply(print, axis=1)
 
 # prompts = ["Give a one word sentiment analysis of the following statement: Pizza is tasty.",
 #            "Create a two-column CSV of top science fiction movies along with the year of release.",
 #            "Create a three-column CSV of top science fiction movies along with the year of release and the total amount grossed.",
 #            "Create a 4-column CSV of top science fiction movies along with the year of release, the total amount grossed, and audience rating."]
 
+
 # Construct the list of different prompts with instructions on what the AI should do with the provided data
-prompts = ["Give a one word sentiment analysis for each statement in a row of the following csv file where there are " + str(df.shape[0]) + " rows so there should only be " + str(df.shape[0]) + " responses: \n" + df.to_csv(index=False, header=False)]
+# prompts = ["Give a one word sentiment analysis for each statement in a row of the following file where each row is seperated by \\r\\n: \n" + df.to_csv(index=False, header=False)]
+# print(prompts)
 
 # Create a for each loop for each individual prompt in prompts. Then, make an API call to the chat bot and save the response
 # for p in prompts:
@@ -32,17 +36,38 @@ prompts = ["Give a one word sentiment analysis for each statement in a row of th
 #         temperature=0.5,
 #         # max_tokens=128
 #     )
-#     print("\n"+p)
+#     print("\n", p)
 #     print(response['choices'][0]['message']['content'])
 #     responses = response['choices'][0]['message']['content']
 
-responses = 'successful, rewarding, harder, positive, pleasant, loved, quick, easy, beautiful, great, lump, specific, comfort, rewarding, favorite, meaningful, enjoyed, favorite, available, favorite, meaningful, incredible, fulfilling, love, favorite, enjoyed, takes, chance, great, low, feeling, important, favorite'
-responses = responses.split(', ')
-print(len(responses))
+# Create an empty list to store responses in and iterate over ever row in the inputted csv file
+responses = []
+for row in df.itertuples():
+    print('\n', row[1])
+    # Construct the list of different prompts with instructions on what the AI should do with the provided data
+    prompts = ["Is this sentance positive or negative in one word: " + row[1]]
+    # Create a for each loop for each individual prompt in prompts. Then, make an API call to the chat bot and save the response
+    for p in prompts:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": p},
+            ],
+            temperature=0.5,
+            # max_tokens=128
+        )
+        print(p)
+        print(response['choices'][0]['message']['content'])
+    responses += [response['choices'][0]['message']['content']]
+
 print(responses)
-responses = responses[:df.shape[0]]
+# responses = 'successful, rewarding, harder, positive, pleasant, loved, quick, easy, beautiful, great, lump, specific, comfort, rewarding, favorite, meaningful, enjoyed, favorite, available, favorite, meaningful, incredible, fulfilling, love, favorite, enjoyed, takes, chance, great, low, feeling, important, favorite'
+# responses = responses.split(', ')
 print(len(responses))
-print(responses)
+# print(responses)
+# responses = responses[:df.shape[0]]
+# print(len(responses))
+# print(responses)
 
 # Save the responses to the pandas dataframe and save the dataframe as a csv file in the files out folder with the same name as the input file
 df['Sentiment'] = responses
